9b01409ae04806a1aecddba267bec623fff1b757
sridharthangella <84458303+sridharthangella@users.noreply.github.com>
2023-04-14 16:39:19 +0530

    Delete TerraformWithAzure directory

--- TerraformWithAzure/IntroBasicDeclarative/Variables.tf
+++
@@ -1,5 +0,0 @@
-variable "replicationType" {
-  description = "The type of replication"
-  type        = string
-  default     = "LRS"
-}
--- TerraformWithAzure/IntroBasicDeclarative/output.tf
+++
@@ -1,3 +0,0 @@
-output "Storage_Account_Name" {
-  value = "${azurerm_storage_account.StorAccount1.name}"
-}
